381ad9ea0830fb4a164eec62686708f24c56b081
PREM-SAGAR1 <premsagarbylla@gmail.com>
2024-12-13 14:02:32 +0530

    SELENIDE SESSION

---
+++ SelenideSessions/src/test/java/org/tests/Dead.java
@@ -0,0 +1,17 @@
+package org.tests;
+
+public class Dead {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hi ojhojhojoj");
+
+		System.out.println("iojhj");
+		
+		System.out.println("hey");
+		
+		System.out.println("hi");
+
+	}
+
+}
